a6d8c7c7b632af4939efdc309efd02f5693cba80
Croustys <akos.barabas2002@gmail.com>
2021-11-19 20:31:07 +0100

    fileName fix

--- script.py
+++ script.py
@@ -3,20 +3,19 @@ import json
 def loadData(filename):
   with open(filename, encoding="utf8") as f:
     data = json.load(f)
-    f.close()
     return data
 
 def main():
   totalData = []
   totalData.append(loadData("./StreamingHistory0.json"))
-  totalData.append(loadData("./StreamingHistory0.json"))
-  totalData.append(loadData("./StreamingHistory0.json"))
+  totalData.append(loadData("./StreamingHistory1.json"))
+  totalData.append(loadData("./StreamingHistory2.json"))
 
   totalMsListenedTo = 0
   for i in totalData:
     for j in i:
       totalMsListenedTo += j["msPlayed"]
-  totalHours = totalMsListenedTo / 1000 / 60 / 60
+  totalHours = totalMsListenedTo / 3600000
   print(f"{totalHours} hours listened to.")
 
 if __name__ == "__main__":
